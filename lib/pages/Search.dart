d7b8e379fd9b483d44aa6b444b8ae9e4d25dd261
Jonathan Loh <loh_jonathan17@hotmail.com>
2022-02-18 18:34:15 +0800

    api query hardcoded, display ok

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Search extends StatefulWidget {
@@ -8,39 +9,86 @@ class Search extends StatefulWidget {
   State<Search> createState() => _SearchState();
 }
 
+Map? data;
+List listdata = [];
+
+Future getQuery() async {
+  try {
+    final response = await http.get(
+      Uri.parse(
+          'https://itunes.apple.com/search?term=jack+johnson&limit=10&entity=album'),
+    );
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      listdata = data!['results'];
+      // print('this is data: $data');
+      // print('this is listdata: $listdata');
+    }
+  } catch (e) {
+    print('caught error: $e');
+  }
+}
+
+@override
 class _SearchState extends State<Search> {
   TextEditingController _queryValue = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 42,
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
-        border: Border.all(color: Colors.black26),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: TextField(
-        controller: _queryValue,
-        decoration: InputDecoration(
-          icon: GestureDetector(
-              child: Icon(Icons.close, color: Colors.black26),
-              onTap: () {
-                var _search = _queryValue.clear();
-              }),
-          suffixIcon: GestureDetector(
-            child: Icon(Icons.search, color: Colors.black26),
-            onTap: () {
-              var _search = _queryValue.text;
-              print('Query ${_search}');
-            },
+    return Column(
+      children: [
+        Container(
+          height: 42,
+          margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+            border: Border.all(color: Colors.black26),
           ),
-          hintText: "Search song here...",
-          border: InputBorder.none,
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: TextField(
+            autofocus: true,
+            controller: _queryValue,
+            decoration: InputDecoration(
+              icon: GestureDetector(
+                  child: Icon(Icons.close, color: Colors.black26),
+                  onTap: () {
+                    var _search = _queryValue.clear();
+                  }),
+              suffixIcon: GestureDetector(
+                child: Icon(Icons.search, color: Colors.black26),
+                onTap: () {
+                  // var _search = _queryValue.text;
+                  // print('Query ${_search}');
+                  getQuery();
+                },
+              ),
+              hintText: "Search song here...",
+              border: InputBorder.none,
+            ),
+          ),
         ),
-      ),
+        Expanded(
+          child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      child: ListTile(
+                        leading: Image.network(listdata[index]['artworkUrl60']),
+                        title: Text(listdata[index]['collectionName'],
+                            style: const TextStyle(
+                                fontSize: 18.0,
+                                overflow: TextOverflow.ellipsis)),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              itemCount: listdata.length),
+        )
+      ],
     );
   }
 }
